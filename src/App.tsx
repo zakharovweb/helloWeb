a47be8c407e0cf092dedebc8c548f346d5697391
Alexander Zakharov <zakharovweb@yandex.ru>
2021-08-25 01:29:25 +0700

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <div className='first-page'>
+        <h3>First Page</h3>
+      </div>
+      <div className='secondary-page'>
+        <h3>Secondary Page</h3>
+      </div>
+    </>
+  )
+}
+
+export { App }
